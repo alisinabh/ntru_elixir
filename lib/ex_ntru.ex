03f0291b95c92886b3d2af07e7c10e3e18dca90e
alisinabh <alisina.bm@gmail.com>
2017-12-19 03:12:10 +0330

    Initial Commit

---
+++ lib/ex_ntru.ex
@@ -0,0 +1,18 @@
+defmodule ExNtru do
+  @moduledoc """
+  Documentation for ExNtru.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExNtru.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ntru_test.exs
@@ -0,0 +1,8 @@
+defmodule ExNtruTest do
+  use ExUnit.Case
+  doctest ExNtru
+
+  test "greets the world" do
+    assert ExNtru.hello() == :world
+  end
+end
